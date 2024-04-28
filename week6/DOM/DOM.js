74e87bba95f80a8166df13cb16bc83506ac147c4
dev-joeysos <joeysos@naver.com>
2024-04-29 00:44:14 +0900

    Revert "feat: Week6 ToDoList(#8)"
    This reverts commit 82c23e0e1fafb76e267b80c927938655d6ead03f.

--- week6/DOM/DOM.js
+++
@@ -1,9 +0,0 @@
-class DOM{
-    constructor(tagName, innerText, className) {
-        this.node = document.createElement(tagName); // html tag
-        this.node.innerText = innerText;
-        if (className) this.node.classList.add(className); // 단순히 추가만 합니다. 
-    }
-}
-
-export default DOM;
